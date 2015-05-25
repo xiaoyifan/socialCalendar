0f0bc6d759b84dbff62f732f4cf8596718c808e1
Yifan Xiao <xiaoyifan@uchicago.edu>
2015-05-25 01:19:36 -0500

    back up for controller file change

---
+++ socialCalendar/eventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  eventsViewController.h
+//  socialCalendar
+//
+//  Created by Yifan Xiao on 5/24/15.
+//  Copyright (c) 2015 Yifan Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface eventsViewController : UITableViewController<PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate>
+
+@end
--- socialCalendar/eventsViewController.m
+++ socialCalendar/eventsViewController.m
@@ -1,40 +1,49 @@
 //
-//  ViewController.m
+//  eventsViewController.m
 //  socialCalendar
 //
-//  Created by Yifan Xiao on 5/19/15.
+//  Created by Yifan Xiao on 5/24/15.
 //  Copyright (c) 2015 Yifan Xiao. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "eventsViewController.h"
 
-@interface ViewController ()
+@interface eventsViewController ()
 
 @end
 
-@implementation ViewController
+@implementation eventsViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 -(void)viewDidAppear:(BOOL)animated{
     
     NSLog(@"%@", [PFUser currentUser]);
- 
+    
     if (![PFUser currentUser]) { // No user logged in
         // Create the log in view controller
         PFLogInViewController *logInViewController = [[PFLogInViewController alloc] init];
         [logInViewController setDelegate:self]; // Set ourselves as the delegate
         logInViewController.fields = (PFLogInFieldsUsernameAndPassword
-                                  | PFLogInFieldsLogInButton
-                                  | PFLogInFieldsSignUpButton
-                                  | PFLogInFieldsPasswordForgotten
-                                  | PFLogInFieldsDismissButton
-                                  | PFLogInFieldsFacebook
-                                  | PFLogInFieldsTwitter);
+                                      | PFLogInFieldsLogInButton
+                                      | PFLogInFieldsSignUpButton
+                                      | PFLogInFieldsPasswordForgotten
+                                      | PFLogInFieldsDismissButton
+                                      | PFLogInFieldsFacebook
+                                      | PFLogInFieldsTwitter);
         // Create the sign up view controller
         PFSignUpViewController *signUpViewController = [[PFSignUpViewController alloc] init];
         [signUpViewController setDelegate:self]; // Set ourselves as the delegate
@@ -48,6 +57,75 @@
     
     
 }
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
 
 #pragma mark - implementation of PFLogInViewControllerDelegate
 
@@ -68,7 +146,7 @@
 
 // Sent to the delegate when a PFUser is logged in.
 - (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user {
-
+    
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
@@ -132,11 +210,6 @@
 // Sent to the delegate when the sign up screen is dismissed.
 - (void)signUpViewControllerDidCancelSignUp:(PFSignUpViewController *)signUpController {
     NSLog(@"User dismissed the signUpViewController");
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
