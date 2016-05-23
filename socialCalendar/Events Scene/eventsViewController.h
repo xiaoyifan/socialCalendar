59615193206a5170ce845aab201183efb9598be8
xiaoyifan <xiaoyifan@uchicago.edu>
2016-05-22 23:15:22 -0700

    removed original Parse delegate methods, added new logout method.

--- socialCalendar/Events Scene/eventsViewController.h
+++ socialCalendar/Events Scene/eventsViewController.h
@@ -9,6 +9,6 @@
 #import <UIKit/UIKit.h>
 
 
-@interface eventsViewController : UITableViewController<PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate>
+@interface eventsViewController : UITableViewController
 
 @end
--- socialCalendar/Events Scene/eventsViewController.m
+++ socialCalendar/Events Scene/eventsViewController.m
@@ -20,10 +20,13 @@
 #import "DismissingAnimator.h"
 #import "expireMarkingView.h"
 #import "SCEventInfoViewController.h"
+#import "AppState.h"
 
 #import "UIColor+CustomColors.h"
 
 #import <QuartzCore/QuartzCore.h>
+
+@import Firebase;
 
 @interface eventsViewController () <AAShareBubblesDelegate, MFMailComposeViewControllerDelegate, UIAlertViewDelegate>
 
@@ -184,113 +187,6 @@
     [self.navigationController pushViewController:eventDetailVC animated:YES];
 }
 
-#pragma mark - implementation of PFLogInViewControllerDelegate
-
-// Sent to the delegate to determine whether the log in request should be submitted to the server.
-- (BOOL)logInViewController:(PFLogInViewController *)logInController shouldBeginLogInWithUsername:(NSString *)username password:(NSString *)password
-{
-    // Check if both fields are completed
-    if (username && password && username.length != 0 && password.length != 0) {
-        return YES; // Begin login process
-    }
-
-    [[[UIAlertView alloc] initWithTitle:@"Missing Information"
-                                message:@"Make sure you fill out all of the information!"
-                               delegate:nil
-                      cancelButtonTitle:@"ok"
-                      otherButtonTitles:nil] show];
-    return NO; // Interrupt login process
-}
-
-// Sent to the delegate when a PFUser is logged in.
-- (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user
-{
-    [self loadDataEvents];
-    [self.tableView reloadData];
-
-    if ([user[@"emailVerified"] isEqual:@1]) {
-        PFInstallation *installation = [PFInstallation currentInstallation];
-        installation[@"user"] = [PFUser currentUser];
-        [installation saveInBackground];
-
-        [self dismissViewControllerAnimated:YES completion:NULL];
-    } else {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Verification Required" message:@"please verify your email first" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-        [alert show];
-    }
-}
-
-// Sent to the delegate when the log in attempt fails.
-- (void)logInViewController:(PFLogInViewController *)logInController didFailToLogInWithError:(NSError *)error
-{
-    NSLog(@"Failed to log in...");
-
-    [[[UIAlertView alloc] initWithTitle:@"Login failed"
-                                message:@"Please make sure you network connection is working."
-                               delegate:nil
-                      cancelButtonTitle:@"ok"
-                      otherButtonTitles:nil] show];
-}
-
-// Sent to the delegate when the log in screen is dismissed.
-- (void)logInViewControllerDidCancelLogIn:(PFLogInViewController *)logInController
-{
-    [self.navigationController popViewControllerAnimated:YES];
-}
-
-#pragma mark - implementation of PFSignUpViewControllerDelegate
-
-// Sent to the delegate to determine whether the sign up request should be submitted to the server.
-- (BOOL)signUpViewController:(PFSignUpViewController *)signUpController shouldBeginSignUp:(NSDictionary *)info
-{
-    BOOL informationComplete = YES;
-
-    // loop through all of the submitted data
-    for (id key in info) {
-        NSString *field = [info objectForKey:key];
-        if (!field || field.length == 0) {
-            // check completion
-            informationComplete = NO;
-            break;
-        }
-    }
-
-    // Display an alert if a field wasn't completed
-    if (!informationComplete) {
-        [[[UIAlertView alloc] initWithTitle:@"Missing Information"
-                                    message:@"Make sure you fill out all of the information!"
-                                   delegate:nil
-                          cancelButtonTitle:@"ok"
-                          otherButtonTitles:nil] show];
-    }
-
-    return informationComplete;
-}
-
-// Sent to the delegate when a PFUser is signed up.
-- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user
-{
-    [self dismissViewControllerAnimated:YES completion:nil]; // Dismiss the PFSignUpViewController
-    [self loadDataEvents];
-
-    if (user[@"emailVerified"] == false) {
-        return;
-    }
-
-    [self.tableView reloadData];
-}
-
-// Sent to the delegate when the sign up attempt fails.
-- (void)signUpViewController:(PFSignUpViewController *)signUpController didFailToSignUpWithError:(NSError *)error
-{
-    NSLog(@"Failed to sign up...");
-}
-
-// Sent to the delegate when the sign up screen is dismissed.
-- (void)signUpViewControllerDidCancelSignUp:(PFSignUpViewController *)signUpController
-{
-    NSLog(@"User dismissed the signUpViewController");
-}
 
 - (IBAction)unwindToMainTable:(UIStoryboardSegue *)segue
 {
@@ -524,9 +420,15 @@
 
 - (IBAction)logoutButtonPressed:(id)sender
 {
-    [PFUser logOut];
-
-    //[self handleLogginAndSignUp];
+    FIRAuth *firebaseAuth = [FIRAuth auth];
+    NSError *signOutError;
+    BOOL status = [firebaseAuth signOut:&signOutError];
+    if (!status) {
+        NSLog(@"Error signing out: %@", signOutError);
+        return;
+    }
+    [AppState sharedInstance].signedIn = false;
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 #pragma mark -- event deleting
